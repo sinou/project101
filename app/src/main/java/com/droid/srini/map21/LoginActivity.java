c6189eda02128f763686cfa2aa8bfd5893db2d64
sinou <srinivasane.arav@gmail.com>
2015-02-16 22:07:36 -0500

    WIP contacts - contains errors!!

--- app/src/main/java/com/droid/srini/map21/LoginActivity.java
+++ app/src/main/java/com/droid/srini/map21/LoginActivity.java
@@ -2,6 +2,7 @@ package com.droid.srini.map21;
 
 import android.app.Fragment;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,7 +15,9 @@ public class LoginActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.login_layout);
+
+        setContentView(R.layout.login_layout);
+
         // Capture our button from layout
         Button button = (Button)findViewById(R.id.signInButton);
         // Register the onClick listener with the implementation above
@@ -25,7 +28,8 @@ public class LoginActivity extends Activity{
     // Create an anonymous implementation of OnClickListener
     private View.OnClickListener mSignInListener = new View.OnClickListener() {
         public void onClick(View v) {
-            setContentView(R.layout.activity_main);
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(i);
         }
     };
 
---
+++ app/src/main/java/com/droid/srini/map21/MyContacts.java
@@ -0,0 +1,60 @@
+package com.droid.srini.map21;
+
+/**
+ * Created by srinivasane on 2/16/2015.
+ */
+
+import android.app.ListActivity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.LinkedList;
+
+public class MyContacts {
+
+    public class MyListActivity extends ListActivity {
+        public void onCreate(Bundle icicle) {
+            super.onCreate(icicle);
+            /*String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                    "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                    "Linux", "OS/2" };*/
+            // use your custom layout
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    R.layout.contacts, R.id.label, getContacts());
+            setListAdapter(adapter);
+        }
+
+        @Override
+        protected void onListItemClick(ListView l, View v, int position, long id) {
+            String item = (String) getListAdapter().getItem(position);
+            Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+        }
+
+        public LinkedList<String> getContacts(){
+            //testing contacts
+            Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
+            LinkedList<String> contacts = new LinkedList<String>();
+                while (phones.moveToNext())
+                {
+
+                    String name=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    contacts.add( name + '(' + phoneNumber + ')' );
+
+                }
+
+            return contacts;
+        }
+
+    }
+
+}
