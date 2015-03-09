3830d393d319fcb2cb7f24893d87f2c5f7d85861
sinou <srinivasane.arav@gmail.com>
2015-03-08 20:14:31 -0400

    Onboarding process completed!

--- app/src/main/java/com/droid/srini/map21/LoginActivity.java
+++ app/src/main/java/com/droid/srini/map21/LoginActivity.java
@@ -4,8 +4,10 @@ import android.app.Fragment;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.provider.ContactsContract;
 import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
@@ -19,14 +21,19 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -35,6 +42,7 @@ import java.util.List;
 public class LoginActivity extends Activity{
 
     private String mPhoneNumber;
+    private String quotes = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,11 +86,23 @@ public class LoginActivity extends Activity{
             HttpClient client = new DefaultHttpClient();
             HttpPost httpPost= new HttpPost(getString(R.string.URL_OTP));
 
-            List<NameValuePair> pairs = new ArrayList<NameValuePair>();
-            pairs.add(new BasicNameValuePair("mobileNumber", mPhoneNumber));
+            JSONObject object = new JSONObject();
+
+            try {
+                object.put("mobileNumber", mPhoneNumber);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            //List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+
+            //pairs.add(new BasicNameValuePair(quotes+"mobileNumber"+quotes, quotes+mPhoneNumber+quotes));
             //pairs.add(new BasicNameValuePair("key2", "value2"));
             try {
-                httpPost.setEntity(new UrlEncodedFormEntity(pairs, HTTP.UTF_8));
+                //httpPost.setEntity(new UrlEncodedFormEntity(pairs, HTTP.UTF_8));
+                StringEntity se = new StringEntity(object.toString());
+                se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                httpPost.setEntity(se);
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
@@ -120,11 +140,20 @@ public class LoginActivity extends Activity{
             HttpPost post= new HttpPost(getString(R.string.URL_verification));
             EditText otpText = (EditText)findViewById(R.id.otpText);
 
-            List<NameValuePair> pairs = new ArrayList<NameValuePair>();
-            pairs.add(new BasicNameValuePair("mobileNumber", mPhoneNumber));
-            pairs.add(new BasicNameValuePair("verificationCode", otpText.getText().toString()));
+            JSONObject object = new JSONObject();
+
             try {
-                post.setEntity(new UrlEncodedFormEntity(pairs));
+                object.put("mobileNumber", mPhoneNumber);
+                object.put("verificationCode", otpText.getText().toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                //post.setEntity(new UrlEncodedFormEntity(pairs));
+                StringEntity se = new StringEntity(object.toString());
+                se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                post.setEntity(se);
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
@@ -138,11 +167,56 @@ public class LoginActivity extends Activity{
 
             StatusLine statusLine = response.getStatusLine();
             if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(i);
+                HttpClient client1 = new DefaultHttpClient();
+                HttpPost httpPost1 = new HttpPost(getString(R.string.URL_update_contacts));
+                JSONObject contactsObject = getContactsJSON();
+                try {
+                    StringEntity se = new StringEntity(contactsObject.toString());
+                    se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                    httpPost1.setEntity(se);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                response = null;
+                try {
+                    response = client.execute(post);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                statusLine = response.getStatusLine();
+                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
+                }
             }
 
         }
     };
+
+    private JSONObject getContactsJSON(){
+
+        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
+
+        ArrayList<JSONObject> contactList = new ArrayList<JSONObject>();
+        JSONObject contacts, contact = new JSONObject();
+        try {
+            while (phones.moveToNext()) {
+
+                String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                contact.put("phoneNumber", phoneNumber);
+                contact.put("name", name);
+                contactList.add(contact);
+                contact = new JSONObject();
+            }
+            contacts = new JSONObject();
+            contacts.put("mobileNumber", mPhoneNumber);
+            contacts.put("contacts", contactList);
+        }catch(JSONException e){
+               //TODO: Handle exception
+        }
+
+        return null;
+    }
 
 }
